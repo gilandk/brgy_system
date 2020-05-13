00a609c420da6743a76f7cdb72b6ba92b6ce2d17
gilandk <gilandk@users.noreply.github.com>
2020-05-13 14:38:21 +0800

    Update
    Officials - logo

--- admin/officials.php
+++ admin/officials.php
@@ -17,8 +17,15 @@ include('include/header.php')
       <h2 class="sub-header text-center">Barangay Officials</h2>
       <br />
       <div class="col-xs-12 col-sm-12 placeholder">
-        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="150" height="150" class="img-responsive" alt="Generic placeholder thumbnail">
-        <h4 class="edit" contenteditable id="name_1"><?php echo $captain['name']; ?></h4>
+      <img src="../uploads/profile/<?php echo $captain['logo']; ?>" width="150" height="150" class="img-responsive" alt="Generic placeholder thumbnail">
+        <form action="update-image(code).php" method="POST" enctype="multipart/form-data">
+        <p> </p>
+        <input type="hidden" name="id_officials" value="9"/>
+        <input type="file" name="image" class="btn" style="margin-left:450px">
+        <p> </p>
+        <button class="btn btn-success">Save</button>
+        </form>
+        <h4 class="edit" contenteditable id="name_9"><?php echo $captain['name']; ?></h4>
         <span contenteditable class="text-muted"><?php echo $captain['position']; ?></span>
       </div>
     <?php
@@ -30,17 +37,59 @@ include('include/header.php')
 
     <?php
 
-    $sql1 = "SELECT * FROM tblofficials WHERE position='Councilor'";
+    $sql1 = "SELECT * FROM tblofficials WHERE position='Councilor' AND id_officials BETWEEN 1 AND 4";
     $result1 = $conn->query($sql1);
     while ($councilor = $result1->fetch_assoc()) {
     ?>
 
       <div class="col-xs-6 col-sm-3 placeholder">
 
-        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="150" height="150" class="img-responsive" alt="Generic placeholder thumbnail">
+      <img src="../uploads/profile/<?php echo $councilor['logo']; ?>" width="150" height="150" class="img-responsive" alt="Generic placeholder thumbnail">
+        
+        <form action="update-image(code).php" method="POST" enctype="multipart/form-data">
+        <p> </p>
+        <input type="hidden" name="id_officials" value="<?php echo $councilor['id_officials']; ?>"/>
+        <input type="file" name="image" class="btn" style="margin-left:30px">
+        <p> </p>
+        <button class="btn btn-success">Save</button>
+        </form>
+
         <h4 class="edit" contenteditable id="name_<?php echo $councilor['id_officials']; ?>"><?php echo $councilor['name']; ?></h4>
         <span class="text-muted"><?php echo $councilor['position']; ?></span><br/>
         <span class="edit" contenteditable class="text-muted" id="title_<?php echo $councilor['id_officials']; ?>"><?php echo $councilor['title']; ?></span>
+        <br/>
+      </div>
+
+    <?php
+    }
+    ?>
+
+  </div>
+
+  <div class="row placeholders">
+
+    <?php
+
+    $sql2 = "SELECT * FROM tblofficials WHERE position='Councilor' AND id_officials BETWEEN 5 AND 8";
+    $result2 = $conn->query($sql2);
+    while ($councilor0 = $result2->fetch_assoc()) {
+    ?>
+
+      <div class="col-xs-6 col-sm-3 placeholder">
+
+      <img src="../uploads/profile/<?php echo $councilor0['logo']; ?>" width="150" height="150" class="img-responsive" alt="Generic placeholder thumbnail">
+        
+        <form action="update-image(code).php" method="POST" enctype="multipart/form-data">
+        <p> </p>
+        <input type="hidden" name="id_officials" value="<?php echo $councilor0['id_officials']; ?>"/>
+        <input type="file" name="image" class="btn" style="margin-left:30px">
+        <p> </p>
+        <button class="btn btn-success">Save</button>
+        </form>
+
+        <h4 class="edit" contenteditable id="name_<?php echo $councilor0['id_officials']; ?>"><?php echo $councilor0['name']; ?></h4>
+        <span class="text-muted"><?php echo $councilor0['position']; ?></span><br/>
+        <span class="edit" contenteditable class="text-muted" id="title_<?php echo $councilor0['id_officials']; ?>"><?php echo $councilor0['title']; ?></span>
         <br/>
       </div>
 
---
+++ admin/update-image(code).php
@@ -0,0 +1,73 @@
+<?php
+
+session_start();
+
+if (empty($_SESSION['id_admin'])) {
+    header("Location: index.php");
+    exit();
+}
+
+require_once('../config.php');
+
+if (isset($_POST)) {
+
+    $msg = '';
+    $id = $_POST['id_officials'];
+
+
+    //profile picture
+    $profileOk = true;
+
+    if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+
+        $folder_dir = "../uploads/profile/";
+
+        $base = basename($_FILES['image']['name']);
+
+        $imageFileType = pathinfo($base, PATHINFO_EXTENSION);
+
+        $profile = uniqid() . "." . $imageFileType;
+
+        $filename = $folder_dir . $profile;
+
+        if (file_exists($_FILES['image']['tmp_name'])) {
+
+            if ($imageFileType == "jpg" || $imageFileType == "png") {
+
+                if ($_FILES['image']['size'] < 500000) { // File size is less than 5MB
+
+                    //If all above condition are met then copy file from server temp location to uploads folder.
+                    move_uploaded_file($_FILES["image"]["tmp_name"], $filename);
+                } else {
+                    $msg = "Wrong Size. Max Size Allowed : 5MB";
+                    echo "<script type='text/javascript'>
+                alert('$msg');
+                window.location.href='officials.php';
+                </script>";
+                }
+            } else {
+                $msg = "Wrong Format. Only jpg & png Allowed";
+                echo "<script type='text/javascript'>
+            alert('$msg');
+            window.location.href='officials.php';
+            </script>";
+            }
+        }
+    } else {
+        $profileOk = false;
+    }
+
+    $sql = "UPDATE tblofficials SET logo='$profile' WHERE id_officials='$id'";
+    $result = $conn->query($sql);
+
+    if ($result === TRUE) {
+        header("location: officials.php");
+        exit();
+    } else {
+        $msg = "Failed";
+        echo "<script type='text/javascript'>
+            alert('$msg');
+            window.location.href='officials.php';
+            </script>";
+    }
+}
